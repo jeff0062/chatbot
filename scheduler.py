ceaf3a3706aba9c30a0a6b0767f3100ad987320d
Jefferson <juniorkd62@gmail.com>
2026-04-24 23:05:58 -0300

    mais  ajuses no fuso horario do scheduler para o servidor

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,6 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from twilio.rest import Client
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timedelta
 from SQL_alchemy import db, Compromisso
 import os
 from dotenv import load_dotenv
@@ -13,7 +13,7 @@ TWILIO_NUMBER = "whatsapp:+14155238886"  # numero sandbox do Twilio
 def verificar_compromissos():
     from app import app
     with app.app_context():
-        agora = datetime.now(timezone.utc) - timedelta(hours=3)
+        agora = datetime.now() - timedelta(hours=3)
         
         compromissos = Compromisso.query.filter_by(concluido=0).all()
         
